73ec068d17373d505d567cf5d74b98e58e15c36f
Guilherme Girardi <girardi.gui@icloud.com>
2020-01-06 08:07:34 +0000

    jQuery functionality finished on navbar

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,16 @@ function closeDropdown() {
   var element = document.getElementsByClassName('open')[0]
   element.classList.remove('open')
 }
+
+/* MULTI LEVEL DROPDOWNS */
+function showsOne(item) {
+  $('.gshow-links').each(function (index) {
+    if ($(this).attr("id") == item) {
+      $(this).show();
+    }
+    else {
+      $(this).hide();
+    }
+  });
+}
+/* MULTI LEVEL DROPDOWNS ENDS */
