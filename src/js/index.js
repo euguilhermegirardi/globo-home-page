0e8c7acb6b4774a0eeb3bae67187bd84bb37fcd6
Guilherme Girardi <girardi.gui@icloud.com>
2020-01-12 12:15:24 +0000

    navbar finished

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,9 @@ function closeDropdown() {
   element.classList.remove('open')
 }
 
-/* MULTI LEVEL DROPDOWNS */
+/* SHOW AND HIDE ITEM */
 function showsOne(item) {
-  $('.links').each(function (index) {
+  $('.showAndHide').each(function (index) {
     if ($(this).attr("id") == item) {
       $(this).show();
     }
@@ -19,4 +19,65 @@ function showsOne(item) {
     }
   });
 }
-/* MULTI LEVEL DROPDOWNS ENDS */
+/* SHOW AND HIDE ITEM ENDS */
+
+/* SHOW AND HIDE ITEM */
+function teams(item) {
+  $('.showTeams').each(function (index) {
+    if ($(this).attr("id") == item) {
+      $(this).show();
+    }
+    else {
+      $(this).hide();
+    }
+  });
+}
+/* SHOW AND HIDE ITEM ENDS */
+
+/* Teams active button */
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+/* Teams active button ends */
+
+/* li active item */
+var header = document.getElementById("myList");
+var items = header.getElementsByClassName("li");
+for (var i = 0; i < items.length; i++) {
+  items[i].addEventListener("mouseover", function () {
+    var current = document.getElementsByClassName("active-item");
+    current[0].className = current[0].className.replace(" active-item", "");
+    this.className += " active-item";
+  });
+}
+/* li active item ends */
+
+/* li active item */
+var header2 = document.getElementById("myList2");
+var items2 = header2.getElementsByClassName("li-2");
+for (var i = 0; i < items2.length; i++) {
+  items2[i].addEventListener("mouseover", function () {
+    var current2 = document.getElementsByClassName("active-item2");
+    current2[0].className = current2[0].className.replace(" active-item2", "");
+    this.className += " active-item2";
+  });
+}
+/* li active item ends */
+
+/* li active item */
+var header3 = document.getElementById("myList3");
+var items3 = header3.getElementsByClassName("li-3");
+for (var i = 0; i < items3.length; i++) {
+  items3[i].addEventListener("mouseover", function () {
+    var current3 = document.getElementsByClassName("active-item3");
+    current3[0].className = current3[0].className.replace(" active-item3", "");
+    this.className += " active-item3";
+  });
+}
+/* li active item ends */
