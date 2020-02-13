8bf06942c347c0f5cc61db503e868b97cce24d83
Guilherme Girardi <girardi.gui@icloud.com>
2020-02-13 08:06:28 +0000

    JS changes

--- src/js/mobileNavbar.js
+++ src/js/mobileNavbar.js
@@ -1,7 +1,22 @@
-function openNavMobile() {
-  document.getElementById("navbar-mobile__menu").style.width = "250px";
-}
+var icon = document.querySelector('.navbar-mobile__menu--open');
+var menu = document.querySelector('.navbar-mobile__menu');
+
+icon.addEventListener('click', function dropDownMenu() {
+  menu.style.width = '250px';
+})
+
+// function openNavMobile() {
+//   document.getElementById("navbar-mobile__menu").style.width = "250px";
+// }
 
 function closeNavMobile() {
   document.getElementById("navbar-mobile__menu").style.width = "0";
+}
+
+function openSubNav() {
+  document.getElementById("navbar-mobile__subSidebar").style.width = "250px";
+}
+
+function closeSubNav() {
+  document.getElementById("navbar-mobile__subSidebar").style.width = "0";
 }
