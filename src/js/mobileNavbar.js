05101fd854e1dcf8f490d90a2ef44b7b380b9901
Guilherme Girardi <girardi.gui@icloud.com>
2020-02-16 11:26:49 +0000

    addEventListeners

--- src/js/mobileNavbar.js
+++ src/js/mobileNavbar.js
@@ -1,22 +1,23 @@
 var icon = document.querySelector('.navbar-mobile__menu--open');
 var menu = document.querySelector('.navbar-mobile__menu');
+var close = document.querySelector('.navbar-mobile__menu--close');
+var link = document.querySelector('.navbar-mobile__links--subMenu');
+var subMenu = document.querySelector('.navbar-mobile__subSidebar');
+var closeSubMenu = document.querySelector('.navbar-mobile__subSidebar--close');
 
-icon.addEventListener('click', function dropDownMenu() {
+icon.addEventListener('click', function() {
   menu.style.width = '250px';
-})
+});
 
-// function openNavMobile() {
-//   document.getElementById("navbar-mobile__menu").style.width = "250px";
-// }
+close.addEventListener('click', function() {
+  menu.style.width = '0'
+});
 
-function closeNavMobile() {
-  document.getElementById("navbar-mobile__menu").style.width = "0";
-}
+link.addEventListener('click', function() {
+  subMenu.style.width = "250px";
+});
 
-function openSubNav() {
-  document.getElementById("navbar-mobile__subSidebar").style.width = "250px";
-}
+closeSubMenu.addEventListener('click', function() {
+  subMenu.style.width = "0";
+});
 
-function closeSubNav() {
-  document.getElementById("navbar-mobile__subSidebar").style.width = "0";
-}
