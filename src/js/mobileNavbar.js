13a3db7013bdd7b3c65ea973d1e3aae46b5424db
Guilherme Girardi <girardi.gui@icloud.com>
2020-04-28 13:27:41 +0100

    BEM review

--- src/js/mobileNavbar.js
+++ src/js/mobileNavbar.js
@@ -1,23 +1,33 @@
-// var icon = document.querySelector('.navbar-mobile__menu--open');
-// var menu = document.querySelector('.navbar-mobile__menu');
-// var close = document.querySelector('.navbar-mobile__menu--close');
-// var link = document.querySelector('.navbar-mobile__links--subMenu');
-// var subMenu = document.querySelector('.navbar-mobile__subSidebar');
-// var closeSubMenu = document.querySelector('.navbar-mobile__subSidebar--close');
+var icon = document.querySelector('.navbar-mobile__menu--open');
+var menu = document.querySelector('.navbar-mobile__menu');
+var close = document.querySelector('.navbar-mobile__menu--close');
+var link = document.querySelector('.navbar-mobile__links--subMenu');
+var subMenu = document.querySelector('.navbar-mobile__subSidebar');
+var closeSubMenu = document.querySelector('.navbar-mobile__subSidebar--close');
 
-// icon.addEventListener('click', function() {
-//   menu.style.width = '250px';
-// });
+if(icon) {
+  icon.addEventListener('click', () => {
+    menu.style.width = '250px';
+  });
+}
 
-// close.addEventListener('click', function() {
-//   menu.style.width = '0'
-// });
+if(close) {
+  close.addEventListener('click', () => {
+    menu.style.width = '0'
+  });
+}
 
-// link.addEventListener('click', function() {
-//   subMenu.style.width = "250px";
-// });
+if(link) {
+  link.addEventListener('click', () => {
+    subMenu.style.width = "250px";
+  });
+}
 
-// closeSubMenu.addEventListener('click', function() {
-//   subMenu.style.width = "0";
-// });
+if(closeSubMenu) {
+  closeSubMenu.addEventListener('click', () => {
+    subMenu.style.width = "0";
+  });
+}
+
+
 
