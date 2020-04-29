fef979440616ac128ddda12c3af9c8b69bfb1959
Guilherme Girardi <girardi.gui@icloud.com>
2020-04-29 12:54:18 +0100

    JS review started - navbar dropdowns done

--- src/js/navbar/open-close-dropdown.js
+++ src/js/navbar/open-close-dropdown.js
@@ -1,9 +1,29 @@
-function openDropdown(e) {
-  var link = e.srcElement.parentElement
-  link.classList.add('open')
-}
+const menuItems = document.querySelectorAll('.navbar__li');
+const dropdowns = document.querySelectorAll('.navbar__dropdown');
 
-function closeDropdown() {
-  var element = document.getElementsByClassName('open')[0]
-  element.classList.remove('open')
+function opened() {
+  menuItems.forEach(element => {
+
+    element.addEventListener('click', () => {
+      // before set the new class, "clear" all others
+      dropdowns.forEach(dropdown => {
+        dropdown.style.display = 'none';
+      });
+
+      let item = document.getElementById(element.dataset.targetId)
+      item.style.display = 'flex';
+    });
+  })
 }
+opened();
+
+function mouseOut() {
+  dropdowns.forEach(element => {
+    element.addEventListener('mouseleave', () => {
+      dropdowns.forEach(dropdown => {
+        dropdown.style.display = 'none';
+      })
+    });
+  })
+}
+mouseOut();
